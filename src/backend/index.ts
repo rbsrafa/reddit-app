2dda5c0d4dd9e13a1ab51715e178f99c14a85b2a
rbsrafa <rbsrafa@gmail.com>
2019-05-01 21:57:17 +0100

    fixed package.json

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 // Config environment variables
 dotenv.config();
 // Define the port to serve the application
-const port = process.env.SERVER_PORT || 3001;
+const port = process.env.PORT || 3001;
 // Create app and initialise server
 (async () => {
   const server = await createApp();
